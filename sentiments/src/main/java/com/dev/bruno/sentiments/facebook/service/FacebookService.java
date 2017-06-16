33a93ba0177523c96424c406b6507f54557e1f0c
Bruno Pacheco <brunopacheco1@yahoo.com>
2017-06-15 22:28:22 -0300

    Ajustando captação do facebook.

--- sentiments/src/main/java/com/dev/bruno/sentiments/facebook/service/FacebookService.java
+++ sentiments/src/main/java/com/dev/bruno/sentiments/facebook/service/FacebookService.java
@@ -13,10 +13,12 @@ import org.apache.commons.io.IOUtils;
 import com.dev.bruno.sentiments.status.helper.JacksonConfig;
 import com.dev.bruno.sentiments.status.service.StatusService;
 
+import facebook4j.Comment;
 import facebook4j.Facebook;
 import facebook4j.FacebookFactory;
+import facebook4j.PagableList;
+import facebook4j.Paging;
 import facebook4j.Post;
-import facebook4j.ResponseList;
 import facebook4j.auth.AccessToken;
 
 @Stateless
@@ -24,33 +26,71 @@ public class FacebookService {
 
 	@Inject
 	private StatusService service;
-	
-	@Resource(name="credentials.folder")
+
+	@Resource(name = "credentials.folder")
 	private String credentialsFolder;
-	
-	@Resource(name="search.filters")
-	private String filters;
+
+	@Resource(name = "facebook.pages")
+	private String pages;
 
 	@SuppressWarnings("unchecked")
 	public void search() throws Exception {
 		String json = new String(IOUtils.toByteArray(new FileInputStream(credentialsFolder + "/facebook.json")));
-		
+
 		Map<String, String> credentials = JacksonConfig.getObjectMapper().readValue(json, HashMap.class);
-		
+
 		Facebook facebook = new FacebookFactory().getInstance();
-		
+
 		facebook.setOAuthAppId(credentials.get("appId"), credentials.get("appSecret"));
-		
+
 		AccessToken accessToken = facebook.getOAuthAppAccessToken();
-		
+
 		facebook.setOAuthAccessToken(accessToken);
-		
-		for(String filter : filters.split(";")) {
-			ResponseList<Post> results = facebook.searchPosts(filter);
+
+		Map<String, Post> allPosts = new HashMap<>();
+
+		for (String page : pages.split(";")) {
+			PagableList<Post> posts = facebook.getPosts(page);
+			Paging<Post> paging;
+			do {
+				for (Post post : posts) {
+					if (post.getMessage() == null) {
+						continue;
+					}
+
+					allPosts.put(post.getId(), post);
+				}
+
+				paging = posts.getPaging();
+			} while ((paging != null) && ((posts = facebook.fetchNext(paging)) != null));
 			
-			for(Post post : results) {
-				service.insert(post.getId(), post.getDescription(), post.getCreatedTime(), "FACEBOOK");
-			}
+			PagableList<Post> taggedPosts = facebook.getTagged(page);
+			paging = null;
+			do {
+				for (Post post : taggedPosts) {
+					if (post.getMessage() == null) {
+						continue;
+					}
+
+					allPosts.put(post.getId(), post);
+				}
+
+				paging = taggedPosts.getPaging();
+			} while ((paging != null) && ((taggedPosts = facebook.fetchNext(paging)) != null));
+		}
+
+		for (Post post : allPosts.values()) {
+			service.insert(post.getId(), post.getMessage(), post.getCreatedTime(), "FACEBOOK");
+
+			PagableList<Comment> comments = post.getComments();
+			Paging<Comment> paging;
+			do {
+				for (Comment comment : comments) {
+					service.insert(comment.getId(), comment.getMessage(), comment.getCreatedTime(), "FACEBOOK");
+				}
+
+				paging = comments.getPaging();
+			} while ((paging != null) && ((comments = facebook.fetchNext(paging)) != null));
 		}
 	}
 }
